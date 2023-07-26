442c4572fdab2c55f80080c931183869e3adf648
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2023-07-26 01:25:13 -0300

    update deco version

--- live.gen.ts
+++ live.gen.ts
@@ -104,24 +104,25 @@ import * as i2$5 from "deco-sites/std/functions/butterCMSPlaces.ts";
 import * as i2$6 from "deco-sites/std/functions/butterCMSPostDetail.ts";
 import * as i2$7 from "deco-sites/std/functions/butterCMSPosts.ts";
 import * as i2$8 from "deco-sites/std/functions/butterCMSRelatedPosts.ts";
-import * as i2$9 from "deco-sites/std/functions/occProductDetailsPage.ts";
-import * as i2$10 from "deco-sites/std/functions/requestToParam.ts";
-import * as i2$11 from "deco-sites/std/functions/shopifyProductDetailsPage.ts";
-import * as i2$12 from "deco-sites/std/functions/shopifyProductList.ts";
-import * as i2$13 from "deco-sites/std/functions/shopifyProductListingPage.ts";
-import * as i2$14 from "deco-sites/std/functions/vndaProductDetailsPage.ts";
-import * as i2$15 from "deco-sites/std/functions/vndaProductList.ts";
-import * as i2$16 from "deco-sites/std/functions/vndaProductListingPage.ts";
-import * as i2$17 from "deco-sites/std/functions/vtexLegacyProductDetailsPage.ts";
-import * as i2$18 from "deco-sites/std/functions/vtexLegacyProductList.ts";
-import * as i2$19 from "deco-sites/std/functions/vtexLegacyProductListingPage.ts";
-import * as i2$20 from "deco-sites/std/functions/vtexLegacyRelatedProductsLoader.ts";
-import * as i2$21 from "deco-sites/std/functions/vtexNavbar.ts";
-import * as i2$22 from "deco-sites/std/functions/vtexProductDetailsPage.ts";
-import * as i2$23 from "deco-sites/std/functions/vtexProductList.ts";
-import * as i2$24 from "deco-sites/std/functions/vtexProductListingPage.ts";
-import * as i2$25 from "deco-sites/std/functions/vtexSuggestions.ts";
-import * as i2$26 from "deco-sites/std/functions/vtexWishlist.ts";
+import * as i2$9 from "deco-sites/std/functions/extractIdFromParam.ts";
+import * as i2$10 from "deco-sites/std/functions/occProductDetailsPage.ts";
+import * as i2$11 from "deco-sites/std/functions/requestToParam.ts";
+import * as i2$12 from "deco-sites/std/functions/shopifyProductDetailsPage.ts";
+import * as i2$13 from "deco-sites/std/functions/shopifyProductList.ts";
+import * as i2$14 from "deco-sites/std/functions/shopifyProductListingPage.ts";
+import * as i2$15 from "deco-sites/std/functions/vndaProductDetailsPage.ts";
+import * as i2$16 from "deco-sites/std/functions/vndaProductList.ts";
+import * as i2$17 from "deco-sites/std/functions/vndaProductListingPage.ts";
+import * as i2$18 from "deco-sites/std/functions/vtexLegacyProductDetailsPage.ts";
+import * as i2$19 from "deco-sites/std/functions/vtexLegacyProductList.ts";
+import * as i2$20 from "deco-sites/std/functions/vtexLegacyProductListingPage.ts";
+import * as i2$21 from "deco-sites/std/functions/vtexLegacyRelatedProductsLoader.ts";
+import * as i2$22 from "deco-sites/std/functions/vtexNavbar.ts";
+import * as i2$23 from "deco-sites/std/functions/vtexProductDetailsPage.ts";
+import * as i2$24 from "deco-sites/std/functions/vtexProductList.ts";
+import * as i2$25 from "deco-sites/std/functions/vtexProductListingPage.ts";
+import * as i2$26 from "deco-sites/std/functions/vtexSuggestions.ts";
+import * as i2$27 from "deco-sites/std/functions/vtexWishlist.ts";
 import * as i2$$0 from "deco-sites/std/accounts/butterCMS.ts";
 import * as i2$$1 from "deco-sites/std/accounts/linxImpulse.ts";
 import * as i2$$2 from "deco-sites/std/accounts/nuvemShop.ts";
@@ -383,24 +384,25 @@ const manifest = {
     "deco-sites/std/functions/butterCMSPostDetail.ts": i2$6,
     "deco-sites/std/functions/butterCMSPosts.ts": i2$7,
     "deco-sites/std/functions/butterCMSRelatedPosts.ts": i2$8,
-    "deco-sites/std/functions/occProductDetailsPage.ts": i2$9,
-    "deco-sites/std/functions/requestToParam.ts": i2$10,
-    "deco-sites/std/functions/shopifyProductDetailsPage.ts": i2$11,
-    "deco-sites/std/functions/shopifyProductList.ts": i2$12,
-    "deco-sites/std/functions/shopifyProductListingPage.ts": i2$13,
-    "deco-sites/std/functions/vndaProductDetailsPage.ts": i2$14,
-    "deco-sites/std/functions/vndaProductList.ts": i2$15,
-    "deco-sites/std/functions/vndaProductListingPage.ts": i2$16,
-    "deco-sites/std/functions/vtexLegacyProductDetailsPage.ts": i2$17,
-    "deco-sites/std/functions/vtexLegacyProductList.ts": i2$18,
-    "deco-sites/std/functions/vtexLegacyProductListingPage.ts": i2$19,
-    "deco-sites/std/functions/vtexLegacyRelatedProductsLoader.ts": i2$20,
-    "deco-sites/std/functions/vtexNavbar.ts": i2$21,
-    "deco-sites/std/functions/vtexProductDetailsPage.ts": i2$22,
-    "deco-sites/std/functions/vtexProductList.ts": i2$23,
-    "deco-sites/std/functions/vtexProductListingPage.ts": i2$24,
-    "deco-sites/std/functions/vtexSuggestions.ts": i2$25,
-    "deco-sites/std/functions/vtexWishlist.ts": i2$26,
+    "deco-sites/std/functions/extractIdFromParam.ts": i2$9,
+    "deco-sites/std/functions/occProductDetailsPage.ts": i2$10,
+    "deco-sites/std/functions/requestToParam.ts": i2$11,
+    "deco-sites/std/functions/shopifyProductDetailsPage.ts": i2$12,
+    "deco-sites/std/functions/shopifyProductList.ts": i2$13,
+    "deco-sites/std/functions/shopifyProductListingPage.ts": i2$14,
+    "deco-sites/std/functions/vndaProductDetailsPage.ts": i2$15,
+    "deco-sites/std/functions/vndaProductList.ts": i2$16,
+    "deco-sites/std/functions/vndaProductListingPage.ts": i2$17,
+    "deco-sites/std/functions/vtexLegacyProductDetailsPage.ts": i2$18,
+    "deco-sites/std/functions/vtexLegacyProductList.ts": i2$19,
+    "deco-sites/std/functions/vtexLegacyProductListingPage.ts": i2$20,
+    "deco-sites/std/functions/vtexLegacyRelatedProductsLoader.ts": i2$21,
+    "deco-sites/std/functions/vtexNavbar.ts": i2$22,
+    "deco-sites/std/functions/vtexProductDetailsPage.ts": i2$23,
+    "deco-sites/std/functions/vtexProductList.ts": i2$24,
+    "deco-sites/std/functions/vtexProductListingPage.ts": i2$25,
+    "deco-sites/std/functions/vtexSuggestions.ts": i2$26,
+    "deco-sites/std/functions/vtexWishlist.ts": i2$27,
   },
   "baseUrl": import.meta.url,
 };
