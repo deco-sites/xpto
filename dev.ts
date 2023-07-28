e3c6a513d431a35976cd2ac3c46e5c75f0c5c6d1
Daniel Moura <daniel-moura@outlook.com>
2023-07-28 13:17:50 -0300

    fix: transform to product sku id and images

--- dev.ts
+++ dev.ts
@@ -2,10 +2,6 @@
 import dev from "$live/dev.ts";
 import liveManifest from "$live/live.gen.ts";
 import liveStdManifest from "deco-sites/std/live.gen.ts";
-import tailwind from "deco-sites/std/tailwindv3.ts";
-
-// Start tailwind background process generation
-tailwind();
 
 // Generate manifest and boot server
 await dev(import.meta.url, "./main.ts", {
