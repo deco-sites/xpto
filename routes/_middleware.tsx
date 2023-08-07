4b33722f71c55983350c256a87c4b4e688afb49d
SACCI <saccilucas@gmail.com>
2023-08-07 13:39:28 -0300

    fix: duplicated token problem resolved

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -118,6 +118,7 @@ export const handler = async (
     clientSecret: "D*HHUrgO2%qADp2JTIUi",
     publicStoreUrl:
       "https://zzte-053.dx.commercecloud.salesforce.com/s/RefArch",
+
     currency: "USD",
     locale: "en-US",
   };
